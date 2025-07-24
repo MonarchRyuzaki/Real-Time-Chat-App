315c380876edfae4f57474dd6bf77d04e4a15f11
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-07-24 17:06:05 +0530

    Wired up middlewares to the routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { register, login, logout } from "../controllers/authContoroller";
+import { validateRegister, validateLogin } from "../middlewares/validation";
+import { verifyToken } from "../middlewares/auth";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
+router.post("/logout", verifyToken, logout);
 
 export default router;
