7777771f2cd584b2c4e926ddb3f54dce4e69941c
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-08-03 18:21:33 +0530

    fix: prisma connection limit increased

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -172,7 +172,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../prisma",
@@ -182,6 +182,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
