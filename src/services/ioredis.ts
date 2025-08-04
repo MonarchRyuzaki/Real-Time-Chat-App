50b7d292da4e87489fda3fc617bc36852f08b623
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-08-04 23:35:28 +0530

    fix: redis connection issue

--- src/services/ioredis.ts
+++ src/services/ioredis.ts
@@ -18,8 +18,12 @@ export async function connectToIoRedis() {
     port: parseInt(process.env.REDIS_PORT),
     username: "default",
     password: process.env.REDIS_PASSWORD,
+    lazyConnect: true, // Prevent automatic connection
   });
+
+  // Manually connect and wait for ready state
   await client.connect();
+
   console.log("Connected to Redis");
   return client;
 }
