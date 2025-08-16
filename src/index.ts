440bd909295cb50a918732269668753a6fc40745
MonarchRyuzaki <shivamganguly2004@gmail.com>
2025-08-16 10:52:44 +0530

    fix: removed the cluster module to stop my pc from crashing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import cluster from "cluster";
 import dotenv from "dotenv";
-import os from "os";
+import { FLUSH_INTERVAL, flushOfflineMessages } from "./queue/offlineWorker";
 import { logger } from "./utils/logger";
 import { gracefulShutdown, setupShutdownHandlers } from "./utils/shutdown";
 import {
@@ -8,10 +7,7 @@ import {
   startHttpServer,
   startWebSocketServers,
 } from "./utils/startup";
-import { FLUSH_INTERVAL, flushOfflineMessages } from "./queue/offlineWorker";
 dotenv.config();
-
-const numCPUs = 1;
 
 export const PORT = 3000;
 export const WS_CHAT_PORT = 4000;
@@ -20,7 +16,7 @@ export const WS_PRESENCE_PORT = 4001;
 async function startServer() {
   try {
     logger.info("🚀 Starting Real-Time Chat Application...");
-    logger.info(`🚀 Worker ${process.pid} is starting...`);
+    logger.info(`🚀 Server ${process.pid} is starting...`);
     await initializeDatabases();
 
     await startHttpServer();
@@ -30,7 +26,7 @@ async function startServer() {
     setInterval(flushOfflineMessages, FLUSH_INTERVAL);
 
     logger.info("✅ All services started successfully");
-    logger.info(`✅ Worker ${process.pid} started all services successfully.`);
+    logger.info(`✅ Server ${process.pid} started all services successfully.`);
     setupShutdownHandlers();
   } catch (error) {
     logger.error("❌ Failed to start server:", error);
@@ -39,23 +35,7 @@ async function startServer() {
   }
 }
 
-if (cluster.isPrimary) {
-  logger.info(`👑 Primary ${process.pid} is running`);
-  logger.info(`Forking for ${numCPUs} CPU cores...`);
-  
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    logger.info(
-      `Worker ${worker.process.pid} died with code: ${code}, signal: ${signal}`
-    );
-    logger.info("Starting a new worker...");
-  });
-} else {
-  startServer().catch((error) => {
-    logger.error("❌ Fatal error starting server:", error);
-    process.exit(1);
-  });
-}
+startServer().catch((error) => {
+  logger.error("❌ Fatal error starting server:", error);
+  process.exit(1);
+});
